030e68891a7cdb6d920026ceb82cc03c462f29ba
Hansimov <591172499@qq.com>
2019-03-24 15:54:31 +0800

    + Finish 0091 with dp

--- leetcode/0091.cpp
+++ leetcode/0091.cpp
@@ -19,8 +19,37 @@ static const auto _ = []() {
 // class Solution {
 // public:
 int numDecodings(string s) {
+    if (s.size()==0) return 0;
+    int dp[s.size()+1]; // dp[i]: num of decodings of s[0:i]
 
-    return s.size();
+    dp[0] = 1;
+    if (s[0]!='0')
+        dp[1] = 1;
+    else
+        return 0;
+
+    string s1 = "123456";
+    for (int i=1; i<s.size(); ++i) {
+        if (s[i] == '0') {
+            if (s[i-1]=='1' || s[i-1]=='2')
+                dp[i+1] = dp[i-1];
+            else
+                return 0;
+        } else
+        if (s1.find(s[i]) != string::npos) {
+            if (s[i-1]=='1' || s[i-1]=='2')
+                dp[i+1] = dp[i] + dp[i-1];
+            else
+                dp[i+1] = dp[i];
+        } else {
+            if (s[i-1]=='1')
+                dp[i+1] = dp[i] + dp[i-1];
+            else
+                dp[i+1] = dp[i];
+        }
+    }
+
+    return dp[s.size()];
 }
 // };
 
