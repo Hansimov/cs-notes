cad12c5e4eb2c40d518e54621b6893159ea813cb
Hansimov <591172499@qq.com>
2019-06-11 20:52:57 +0800

    + Finish 0152

--- leetcode/0152.cpp
+++ leetcode/0152.cpp
@@ -14,10 +14,22 @@ using namespace std;
 // public:
 int maxProduct(vector<int>& nums) {
     if (nums.empty()) return 0;
+    
+    int n = nums.size();
+    //tmax when i: max product of subarray ends with nums[i]
+    //tmin when i: min product of subarray ends with nums[i] (<=0)
+    int tmin = nums[0]; 
+    int tmax = nums[0];
+    int res = tmax;
 
-    
-    
-    return 1;
+    for (int i=1; i<n; ++i) {
+        if (nums[i]<0) swap(tmin,tmax);
+        tmax = max(nums[i], tmax*nums[i]);
+        tmin = min(nums[i], tmin*nums[i]);
+        res = max(res, tmax);
+    }
+
+    return res;
 }
 // };
 
@@ -39,13 +51,11 @@ int main() {
         vector<int> vni = {};
         while (ss >> d) {
             vni.push_back(d);
-            // printf("%d ", d);
         }
         vn.push_back(vni);
 
         getline(fp, rin, '\n');
         vr.push_back(stoi(rin.c_str(),NULL,10));
-        // ++c;printf("==%d==\n", c);
     }
     fp.close();
 
@@ -53,6 +63,7 @@ int main() {
     printf("--- --- --- --- \n");
     int correct_cnt=0;
     for (int i=0; i<vs.size(); ++i) {
+        // if (i+1!=2) continue;
         int res = maxProduct(vn[i]);
         bool is_correct = res==vr[i];
         printf("%d\t%d\t%d\t%d\n", is_correct, 2*i+1, vr[i], res);
