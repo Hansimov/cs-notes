922b0d1bc3559f2bcf0fef361bafdbb1c95c40f4
Hansimov <591172499@qq.com>
2019-08-23 16:32:26 +0800

    + Finish 0136 with xor

---
+++ leetcode/0136.cpp
@@ -0,0 +1,69 @@
+/*
+- Easy Java solution (tell you why using bitwise XOR) - LeetCode Discuss 
+    https://leetcode.com/problems/single-number/discuss/43201/Easy-Java-solution-(tell-you-why-using-bitwise-XOR)
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <limits.h>
+
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int res = 0;
+    for(int num:nums) res ^= num;
+    return res;
+}
+
+string filename = "0136.txt";
+const auto& myFunc = singleNumber;
+
+int main() {
+    string sin, rin;
+    vector<string> vs;
+    vector<vector<int>> vn;
+    vector<int> vr;
+
+    ifstream fp;
+    fp.open(filename);
+
+    int d;
+    while (getline(fp, sin, '\n')) {
+        vs.push_back(sin);
+        stringstream ss(sin);
+
+        vector<int> vni = {};
+        while (ss >> d) {
+            vni.push_back(d);
+            // printf("%d ", d);
+        }
+        vn.push_back(vni);
+
+        getline(fp, rin, '\n');
+        vr.push_back(stoi(rin.c_str(),NULL,10));
+    }
+    fp.close();
+
+    printf("%s\t%s\t%s\t%s\n", "T","L", "√", "?");
+    printf("--- --- --- --- \n");
+    int correct_cnt=0;
+    for (int i=0; i<vs.size(); ++i) {
+        int res = myFunc(vn[i]);
+        bool is_correct = res==vr[i];
+        printf("%d\t%d\t%d\t%d\n", is_correct, 2*i+1, vr[i], res);
+        if (is_correct) ++correct_cnt;
+    }
+    printf("--- --- --- --- \n");
+    printf("%s\t%d/%d\n", correct_cnt==vr.size()?"√":"×",correct_cnt, vr.size());
+
+    return 0;
+}
---
+++ leetcode/0446.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <limits.h>
+
+using namespace std;
+
+int numberOfArithmeticSlices(vector<int>& A) {
+    
+    return 0;
+}
+
+string filename = "0446.txt";
+const auto& myFunc = numberOfArithmeticSlices;
+
+int main() {
+    string sin, rin;
+    vector<string> vs;
+    vector<vector<int>> vn;
+    vector<int> vr;
+
+    ifstream fp;
+    fp.open(filename);
+
+    int d;
+    while (getline(fp, sin, '\n')) {
+        vs.push_back(sin);
+        stringstream ss(sin);
+
+        vector<int> vni = {};
+        while (ss >> d) {
+            vni.push_back(d);
+            // printf("%d ", d);
+        }
+        vn.push_back(vni);
+
+        getline(fp, rin, '\n');
+        vr.push_back(stoi(rin.c_str(),NULL,10));
+    }
+    fp.close();
+
+    printf("%s\t%s\t%s\t%s\n", "T","L", "√", "?");
+    printf("--- --- --- --- \n");
+    int correct_cnt=0;
+    for (int i=0; i<vs.size(); ++i) {
+        int res = myFunc(vn[i]);
+        bool is_correct = res==vr[i];
+        printf("%d\t%d\t%d\t%d\n", is_correct, 2*i+1, vr[i], res);
+        if (is_correct) ++correct_cnt;
+    }
+    printf("--- --- --- --- \n");
+    printf("%s\t%d/%d\n", correct_cnt==vr.size()?"√":"×",correct_cnt, vr.size());
+
+    return 0;
+}
