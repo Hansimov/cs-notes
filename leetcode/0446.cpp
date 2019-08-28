b2cc3bb560d8fc8a3d9a58f4e5a40ae115eda35e
Hansimov <591172499@qq.com>
2019-08-28 14:56:46 +0800

    + Finish 0446 [Arithmetic Slices II - Subsequence] with dp using unordered_map and unordered_set

--- leetcode/0446.cpp
+++ leetcode/0446.cpp
@@ -1,16 +1,24 @@
 /*
-- 8-line O(N^2) solution without long type conversion (with key points and comments) - LeetCode Discuss 
-    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92838/8-line-O(N2)-solution-without-long-type-conversion-(with-key-points-and-comments)
-- Detailed explanation for Java O(n^2) solution - LeetCode Discuss 
-    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92822/Detailed-explanation-for-Java-O(n2)-solution
-- [LeetCode] Arithmetic Slices II - Subsequence 算数切片之二 - 子序列 - Grandyang - 博客园 
-    https://www.cnblogs.com/grandyang/p/6057934.html
-- O(N^2) MLE/TLE in C++? Try this one, Concise and Fast. - LeetCode Discuss 
+= O(N^2) MLE/TLE in C++? Try this one, Concise and Fast. - LeetCode Discuss 
     https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92850/O(N2)-MLETLE-in-C%2B%2B-Try-this-one-Concise-and-Fast.
-- 11 line Python O(n^2) solution - LeetCode Discuss 
+= Detailed explanation for Java O(n^2) solution - LeetCode Discuss 
+    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92822/Detailed-explanation-for-Java-O(n2)-solution
+= 200ms C++ DP solution - LeetCode Discuss 
+    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92851/200ms-C%2B%2B-DP-solution
+= 8-line O(N^2) solution without long type conversion (with key points and comments) - LeetCode Discuss 
+    https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92838/8-line-O(N2)-solution-without-long-type-conversion-(with-key-points-and-comments)
+= [LeetCode] Arithmetic Slices II - Subsequence 算数切片之二 - 子序列 - Grandyang - 博客园 
+    https://www.cnblogs.com/grandyang/p/6057934.html
+= 11 line Python O(n^2) solution - LeetCode Discuss 
     https://leetcode.com/problems/arithmetic-slices-ii-subsequence/discuss/92852/11-line-Python-O(n2)-solution
-- map和unordered_map的差别和使用 - 陈云佳的专栏 - CSDN博客 
+= map和unordered_map的差别和使用 - 陈云佳的专栏 - CSDN博客 
     https://blog.csdn.net/BillCYJ/article/details/78985895
+= C++语言STL库中unordered_map容器的基本用法-百度经验 
+    https://jingyan.baidu.com/article/60ccbcebad34e964cab19720.html
+= unordered_set::unordered_set - C++ Reference 
+    http://www.cplusplus.com/reference/unordered_set/unordered_set/unordered_set/
+= unordered_set::count - C++ Reference 
+    http://www.cplusplus.com/reference/unordered_set/unordered_set/count/
 */
 
 #include <iostream>
@@ -32,17 +40,20 @@
 using namespace std;
 
 int numberOfArithmeticSlices(vector<int>& A) {
-    if (A.empty()) return 0;
     int n = A.size();
-    vector<unordered_map<long long, int >> dp(n);
+    if (n==0) return 0;
+    vector<unordered_map<long long,int>> dp(n);
+    // dp[i][d] = the number of arithmetic subsequences whose last but one number is A[i], difference is d.
     unordered_set<int> s(A.begin(), A.end());
+
     int res = 0;
-    for (int i = 1; i < n; ++i) {
-        for (int j = i-1; j >= 0; --j) {
+    for (int i=1; i<n; ++i) {
+        for (int j=i-1; j>=0; --j) {
             long long d = (long long)A[i] - (long long)A[j];
-            int tmp = dp[j].count(d) ? dp[j][d] : 0;
-            if (tmp) res += tmp;
-            if (s.count(A[i]+d)) dp[i][d] += 1 + tmp;
+            int prevNum = dp[j].count(d) ? dp[j][d] : 0;
+            res += prevNum;
+            if (s.count(A[i]+d))
+                dp[i][d] += 1 + prevNum;
         }
     }
     return res;
