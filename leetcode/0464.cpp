e52723602feb99402437d07a16ad1e05decb3849
Hansimov <591172499@qq.com>
2019-08-30 16:19:39 +0800

    + Finish 0464 [Can I Win] with dfs

--- leetcode/0464.cpp
+++ leetcode/0464.cpp
@@ -1,4 +1,9 @@
 /*
+= 7-liner C++ beat 98.4%, DFS with early termination check (detailed explanation) - LeetCode Discuss 
+    https://leetcode.com/problems/can-i-win/discuss/95320/7-liner-C%2B%2B-beat-98.4-DFS-with-early-termination-check-(detailed-explanation)
+    https://leetcode.com/problems/can-i-win/discuss/95320/7-liner-C++-beat-98.4-DFS-with-early-termination-check-(detailed-explanation)/160463
+    https://leetcode.com/problems/can-i-win/discuss/95320/7-liner-C++-beat-98.4-DFS-with-early-termination-check-(detailed-explanation)/127969
+    https://leetcode.com/problems/can-i-win/discuss/95320/7-liner-C++-beat-98.4-DFS-with-early-termination-check-(detailed-explanation)/238059
 = Java solution using HashMap with detailed explanation - LeetCode Discuss 
     https://leetcode.com/problems/can-i-win/discuss/95277/Java-solution-using-HashMap-with-detailed-explanation
 */
@@ -21,15 +26,77 @@
 
 using namespace std;
 
-bool canIWin(int maxChoosableInteger, int desiredTotal) {
-    int m = maxChoosableInteger;
-    int d = desiredTotal;
+enum RESULT {UNKNOWN, WIN, LOSE};
+// dfs(~) : true: there *exists* a choice which can make current player win
+//          false: current player can not win no matter what choice he makes
+bool dfs(int max, int t, vector<RESULT> &memo, int k=0) {
+    if (t<=0) // the opponent has reached desiredTotal at last step
+        return false;
+    if (memo[k] != UNKNOWN)
+        return memo[k]==WIN;
 
-    if ((m+1)*m/2<d) return false;
-    if (m>=d) return true;
+    // i-th bit of k (base 2): 0: not picked; 1: picked
+    for (int i=1; i<=max; ++i) {
+        bool isIthNumPicked = (k&1<<i);
+        if (isIthNumPicked)
+            continue;
 
+        int kUpdatedAfterIthNumPicked = k|(1<<i);
+        bool isOpponentHasChanceToWin = dfs(max,t-i,memo,kUpdatedAfterIthNumPicked);
+        if (!isOpponentHasChanceToWin) {
+            memo[k]=WIN;
+            return true;
+        }
+    }
+    // for any choice of current player, the opponent has a chance to win, so the current player can not win
+    memo[k] = LOSE;
     return false;
 }
+
+bool canIWin(int maxChoosableInteger, int desiredTotal) {
+    int max = maxChoosableInteger;
+    int t = desiredTotal;
+
+    if (max>t) return true;
+    int sum = max*(max+1)/2;
+    if (sum<t) return false;
+    // if (sum==t) return max%2;
+
+    vector<RESULT> memo(1 << (max+1), UNKNOWN);
+    return dfs(max, t, memo);
+}
+
+
+// // class Solution {
+//     enum RESULT { UNKNOWN, WIN, LOSE };
+// // public:
+//     bool dfs(int max, int t, vector<RESULT> &memo, int k = 0) {
+//         if (t <= 0) return false;
+//         if (memo[k] != UNKNOWN) return memo[k] == WIN;
+
+//         for (int i = 1; i <= max; i++) {
+//             if (!(k & 1 << i) && !dfs(max, t - i, memo, k | (1 << i))) {
+//                 memo[k] = WIN;
+//                 return true;
+//             }
+//         }
+//         memo[k] = LOSE;
+//         return false;
+//     }
+//     bool canIWin(int maxChoosableInteger, int desiredTotal) {
+//         if (maxChoosableInteger >= desiredTotal) return true;
+
+//         int s = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
+//         if (s < desiredTotal) return false;
+//         // if (s == desiredTotal) return maxChoosableInteger % 2;
+    
+//         vector<RESULT> memo(1 << (maxChoosableInteger + 1), UNKNOWN);
+
+//         return dfs(maxChoosableInteger, desiredTotal, memo);
+//     }
+// private:
+
+// };
 
 string filename = "0464.txt";
 const auto& myFunc = canIWin;
