09fa906404d9d4c0c9f03f2cdf3725b06ec8f100
Hansimov <591172499@qq.com>
2019-08-30 16:28:42 +0800

    + Modify 0464 [Can I Win] from vector to unordered_map, save space but increase time

--- leetcode/0464.cpp
+++ leetcode/0464.cpp
@@ -29,10 +29,11 @@ using namespace std;
 enum RESULT {UNKNOWN, WIN, LOSE};
 // dfs(~) : true: there *exists* a choice which can make current player win
 //          false: current player can not win no matter what choice he makes
-bool dfs(int max, int t, vector<RESULT> &memo, int k=0) {
+bool dfs(int max, int t, unordered_map<int,RESULT> &memo, int k=0) {
     if (t<=0) // the opponent has reached desiredTotal at last step
         return false;
-    if (memo[k] != UNKNOWN)
+    // if (memo[k] != UNKNOWN)
+    if (memo.count(k))
         return memo[k]==WIN;
 
     // i-th bit of k (base 2): 0: not picked; 1: picked
@@ -62,41 +63,11 @@ bool canIWin(int maxChoosableInteger, int desiredTotal) {
     if (sum<t) return false;
     // if (sum==t) return max%2;
 
-    vector<RESULT> memo(1 << (max+1), UNKNOWN);
+    // vector<RESULT> memo(1 << (max+1), UNKNOWN);
+    unordered_map<int,RESULT> memo;
     return dfs(max, t, memo);
 }
 
-
-// // class Solution {
-//     enum RESULT { UNKNOWN, WIN, LOSE };
-// // public:
-//     bool dfs(int max, int t, vector<RESULT> &memo, int k = 0) {
-//         if (t <= 0) return false;
-//         if (memo[k] != UNKNOWN) return memo[k] == WIN;
-
-//         for (int i = 1; i <= max; i++) {
-//             if (!(k & 1 << i) && !dfs(max, t - i, memo, k | (1 << i))) {
-//                 memo[k] = WIN;
-//                 return true;
-//             }
-//         }
-//         memo[k] = LOSE;
-//         return false;
-//     }
-//     bool canIWin(int maxChoosableInteger, int desiredTotal) {
-//         if (maxChoosableInteger >= desiredTotal) return true;
-
-//         int s = maxChoosableInteger * (maxChoosableInteger + 1) / 2;
-//         if (s < desiredTotal) return false;
-//         // if (s == desiredTotal) return maxChoosableInteger % 2;
-    
-//         vector<RESULT> memo(1 << (maxChoosableInteger + 1), UNKNOWN);
-
-//         return dfs(maxChoosableInteger, desiredTotal, memo);
-//     }
-// private:
-
-// };
 
 string filename = "0464.txt";
 const auto& myFunc = canIWin;
