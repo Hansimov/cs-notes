d2f2de01f6519c240d9dd5a824ebab392b54db71
Hansimov <591172499@qq.com>
2019-09-05 16:31:42 +0800

    + Finish [0004 - Median of Two Sorted Arrays] with binary search

--- leetcode/0004.cpp
+++ leetcode/0004.cpp
@@ -22,8 +22,32 @@
 using namespace std;
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    
-    return 0.0;
+    // nums1 is shorter than nums2
+    int N1 = nums1.size();
+    int N2 = nums2.size();
+
+    if (N1>N2) return findMedianSortedArrays(nums2,nums1);
+
+    int lo = 0;
+    int hi = N1*2;
+
+    while (lo <= hi) {
+        int mid1 =  (lo+hi)/2;
+        int mid2 = N1+N2-mid1;
+
+        double L1 = (mid1==0) ? INT_MIN : nums1[(mid1-1)/2];
+        double L2 = (mid2==0) ? INT_MIN : nums2[(mid2-1)/2];
+        double R1 = (mid1==N1*2) ? INT_MAX : nums1[mid1/2];
+        double R2 = (mid2==N2*2) ? INT_MAX : nums2[mid2/2];
+
+        if (L1>R2)
+            hi = mid1-1;
+        else if (L2>R1)
+            lo = mid1+1;
+        else
+            return (max(L1,L2)+min(R1,R2))/2;
+    }
+    return 0;
 }
 
 string filename = "0004.txt";
