bb67d6b75142945620f43dc296962b723c4a6728
Hansimov <591172499@qq.com>
2019-09-16 13:58:34 +0800

    + Finish [0154 Find Minimum in Rotated Sorted Array II] with binary search

---
+++ leetcode/0154.cpp
@@ -0,0 +1,85 @@
+/*
+= Beats 100% Binary Search with Explanations - LeetCode Discuss 
+    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/discuss/167981/Beats-100-Binary-Search-with-Explanations
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <limits.h>
+
+using namespace std;
+
+int findMin(vector<int>& v) {
+    int n = v.size();
+    if (n==0) return -1;
+
+    int l=0, r=n-1, m;
+    while (l<r) {
+        m=l+(r-l)/2;
+        if (v[m]>v[r])
+            l = m+1;
+        else if (v[l]>v[m]) {
+            r = m;
+            l++;
+        } else { // v[l]<=v[m]<v[r]
+            r--;
+        }
+    }
+    return v[l];
+}
+
+string filename = "0154.txt";
+const auto& myFunc = findMin;
+
+int main() {
+    string sin, rin;
+    vector<string> vs;
+    vector<vector<int>> vn;
+    vector<int> vr;
+
+    ifstream fp;
+    fp.open(filename);
+
+    int d;
+    while (getline(fp, sin, '\n')) {
+        vs.push_back(sin);
+        stringstream ss(sin);
+
+        vector<int> vni = {};
+        while (ss >> d) {
+            vni.push_back(d);
+            // printf("%d ", d);
+        }
+        vn.push_back(vni);
+
+        getline(fp, rin, '\n');
+        vr.push_back(stoi(rin.c_str(),NULL,10));
+    }
+    fp.close();
+
+    printf("%s\t%s\t%s\t%s\n", "T","L", "√", "?");
+    printf("--- --- --- --- \n");
+    int correct_cnt=0;
+    for (int i=0; i<vs.size(); ++i) {
+        int res = myFunc(vn[i]);
+        bool is_correct = res==vr[i];
+        printf("%s\t%d\t%d\t%d\n", is_correct?" ":"x", 2*i+1, vr[i], res);
+        if (is_correct) ++correct_cnt;
+    }
+    printf("--- --- --- --- \n");
+    printf("%s\t%d/%d\n", correct_cnt==vr.size()?"√":"×",correct_cnt, vr.size());
+
+    return 0;
+}
