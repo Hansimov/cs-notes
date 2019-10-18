dd373bf7c6c4804255d3252ef9dc3963f55915ae
Hansimov <591172499@qq.com>
2019-10-18 18:48:27 +0800

    + Finish [275 - H-Index II] with binary search

---
+++ leetcode/0275.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <set>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <limits.h>
+
+using namespace std;
+
+int hIndex(vector<int>& citations) {
+    int n = citations.size();
+    if (n==0) return 0;
+    int l=0,r=n-1,mid;
+    while (l<=r) {
+        mid = l+(r-l)/2;
+        if (citations[mid]==n-mid)
+            return n-mid;
+        else if (citations[mid]>n-mid)
+            r = mid-1;
+        else
+            l = mid+1;
+    }
+    return n-l;
+}
+
+string filename = "0275.txt";
+const auto& myFunc = hIndex;
+
+int main() {
+    string sin, rin;
+    vector<string> vs;
+    vector<vector<int>> vn;
+    vector<int> vr;
+
+    ifstream fp;
+    fp.open(filename);
+
+    int d;
+    while (getline(fp, sin, '\n')) {
+        vs.push_back(sin);
+        stringstream ss(sin);
+
+        vector<int> vni = {};
+        while (ss >> d) {
+            vni.push_back(d);
+            // printf("%d ", d);
+        }
+        vn.push_back(vni);
+
+        getline(fp, rin, '\n');
+        vr.push_back(stoi(rin.c_str(),NULL,10));
+    }
+    fp.close();
+
+    printf("%s\t%s\t%s\t%s\n", "T","L", "√", "?");
+    printf("--- --- --- --- \n");
+    int correct_cnt=0;
+    for (int i=0; i<vs.size(); ++i) {
+        int res = myFunc(vn[i]);
+        bool is_correct = res==vr[i];
+        printf("%s\t%d\t%d\t%d\n", is_correct?" ":"x", 2*i+1, vr[i], res);
+        if (is_correct) ++correct_cnt;
+    }
+    printf("--- --- --- --- \n");
+    printf("%s\t%d/%d\n", correct_cnt==vr.size()?"√":"×",correct_cnt, vr.size());
+
+    return 0;
+}
