9f8c009096959c76b89670bac00776b9c987ed95
Hansimov <591172499@qq.com>
2020-04-17 11:58:14 +0800

    + Finish 0257 with recursion

--- leetcode/0257 Binary Tree Paths.cpp
+++ leetcode/0257 Binary Tree Paths.cpp
@@ -1,3 +1,6 @@
+// * C++ simple 4ms recursive solution - LeetCode Discuss 
+// ** https://leetcode.com/problems/binary-tree-paths/discuss/68270/C%2B%2B-simple-4ms-recursive-solution
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,7 +13,30 @@
 
 class Solution {
 public:
+    void helper(vector<string>& res, TreeNode* t, string s) {
+        if (t!=NULL)
+            s += to_string(t->val);
+
+        if (t->left==NULL && t->right==NULL) {
+            res.push_back(s);
+            return ;
+        } else {
+            s += "->";
+        }
+
+        if (t->left!=NULL)
+            helper(res, t->left, s);
+        if (t->right!=NULL)
+            helper(res, t->right, s);
+    }
+
     vector<string> binaryTreePaths(TreeNode* root) {
-        
+        vector<string> res;
+
+        if (root==NULL)
+            return res;
+
+        helper(res, root, "");
+        return res;
     }
 };
