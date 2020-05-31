74b74f262bb1743dd4cc52ed9f045a8077228406
Hansimov <591172499@qq.com>
2020-05-31 22:57:23 +0800

    + Update tree leetcode problems

--- cs-seminar/算法与数据结构/parse_problems_links.py
+++ cs-seminar/算法与数据结构/parse_problems_links.py
@@ -23,7 +23,7 @@ tr_L = re.findall(r"<tr[\s\S]*?</tr>", text)
 
 md_head = "编号 | 题目 | 通过率 / 难度\n"
 md_sep  = "---|---|---\n"
-md_line_bd = "{} | [{}]({}) [中文站]({}) | {}% / {}\n"
+md_line_bd = "{} |[中文版]({}) / [{}]({}) | {}% / {}\n"
 md_str = md_head + md_sep
 with open(out_fname, "w") as wf:
     for tr in tr_L[:]:
@@ -35,7 +35,7 @@ with open(out_fname, "w") as wf:
         link_cn = link_cn_bd.format(re.findall(r'href="(.*?)"',td_L[2])[0])
         acceptance = re.findall(r'value="([\s\S]*?)"',td_L[3])[0]
         difficulty = re.findall(r'<span.*>([\s\S]*?)</span>',td_L[4])[0]
-        md_str += md_line_bd.format(idx, title, link_en, link_cn, acceptance, difficulty)
+        md_str += md_line_bd.format(idx, link_cn, title, link_en, acceptance, difficulty)
     # print(md_str)
     wf.write(md_str)
         
