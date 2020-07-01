3153709a4f1fcbf3e0225204ec3c4ffdd4bf8b02
Hansimov <591172499@qq.com>
2020-07-01 10:56:28 +0800

    + Finish [0135 分发糖果]

---
+++ leetcode/0135 分发糖果.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        if (ratings.size()==0)
+            return 0;
+
+        vector<int> nums(ratings.size(), 1);
+
+        for (int i=1; i<nums.size(); ++i) {
+            if (ratings[i]>ratings[i-1])
+                nums[i] = nums[i-1] + 1;
+        }
+        for (int i=nums.size()-2; i>=0; --i) {
+            if (ratings[i]>ratings[i+1]) {
+                nums[i] = max(nums[i], nums[i+1]+1);
+            }
+        }
+        return accumulate(nums.begin(), nums.end(), 0);
+    }
+};
