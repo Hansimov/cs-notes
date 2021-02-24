5109924942f673f34a385a6b426d77aff6104a10
asimov <591172499@qq.com>
2021-02-25 01:08:10 +0800

    + Finish jz-60

--- 面试高频题/JZ-60 n个骰子的点数.cpp
+++ 面试高频题/JZ-60 n个骰子的点数.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     vector<double> dicesProbability(int n) {
-        vector<vector<int>> dp(n, vector<int>(n*6));
-        for 
+        vector<int> dp(6*n+1, 0);
+        for (int i=1; i<=6; ++i) {
+            dp[i] = 1;
+        }
+        
+        // i: row num; j: col idx; k: 1~6(max)
+        for (int i=2; i<=n; ++i) {
+            for (int j=6*i; j>=i; --j) {
+                dp[j]=0;
+                for (int k=1; k<=6 && j-k>=i-1; ++k) {
+                    dp[j] += dp[j-k];
+                }
+            }
+        }
+
+        double div = pow(6,n);
+        vector<double> res;
+        for (int i=n; i<=6*n; ++i) {
+            res.push_back(dp[i]*1.0 / div);
+        }
+
+        return res;
     }
 };
